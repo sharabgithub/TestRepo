9422d12edd3d29c76b813f8066e2460cf932d4f8
sharabgithub <34997621+sharabgithub@users.noreply.github.com>
2022-03-17 13:53:49 +0530

    Conflic change
    Done

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Hello World! - this is Prasanna testing github desktop" );
         System.out.println( "Line 2 is added from local branch" );
         System.out.println( "Line 3 is added from Github" );
+        System.out.println( "Line Conflict added from Github" );
     }
 }
 // this is added by prasanna
