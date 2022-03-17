d09b73d6fd18b552f94edcd86201179a4951e631
sharabgithub <34997621+sharabgithub@users.noreply.github.com>
2022-03-17 23:11:36 +0530

    YY GH

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,6 +12,8 @@ public class App
         System.out.println( "Line 2 is added from local branch" );
         System.out.println( "Line 3 is added from Github" );
         System.out.println( "Line Conflict added from Github" );
+        
+        System.out.println( "YYYYYYYYYYYYY GH YYYYYYYYYYYYYYYYYY" );
     }
 }
 // this is added by prasanna
