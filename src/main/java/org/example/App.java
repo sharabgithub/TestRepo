25f77d10b651a3b6a177276679dcb86dbc79eae1
sharabgithub <34997621+sharabgithub@users.noreply.github.com>
2022-03-17 13:31:14 +0530

    add from GH
    update App java from GH itself.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World! - this is Prasanna testing github desktop" );
         System.out.println( "Line 2 is added from local branch" );
+        System.out.println( "Line 3 is added from Github" );
     }
 }
 // this is added by prasanna
