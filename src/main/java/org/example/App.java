bf44c052d2c6755e92c67d4817c41537313a2391
sharabgithub <hiprasanna.s@gmail.com>
2022-03-17 22:57:11 +0530

    add from GH
    update App java from GH itself.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,6 +12,7 @@ public class App
         System.out.println( "Line 2 is added from local branch" );
         System.out.println( "Line 3 is added from Github" );
         System.out.println( "Line Conflict added from Github" );
+        System.out.println( "Line 4 is added from Local IJ" );
     }
 }
 // this is added by prasanna
